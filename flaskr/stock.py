4b4830128dfde7984f4d9cc4073daf78219c9d29
Stefannsen <stefan.negulescu@my.fmi.unibuc.ro>
2022-01-28 18:13:24 +0200

    Update stock.py

--- flaskr/stock.py
+++ flaskr/stock.py
@@ -37,14 +37,14 @@ def set_stock():
         'SELECT id, product_name, quantity, product_expiration_date, shelf_number'
         ' FROM stock'
         ' ORDER BY product_expiration_date DESC'
-    ).fetchone()
+    ).fetchall()
     return jsonify({
         'status': 'Stock successfully recorded/retrieved',
-        'data': {
-            'id': check['id'],
-            'product_name': check['product_name'],
-            'quantity': check['quantity'],
-            'product_expiration_date': check['product_expiration_date'],
-            'shelf_number': check['shelf_number'],
-        }
+        'data': [{
+            'id': check[i]['id'],
+            'product_name': check[i]['product_name'],
+            'quantity': check[i]['quantity'],
+            'product_expiration_date': check[i]['product_expiration_date'],
+            'shelf_number': check[i]['shelf_number']} for i in range(len(check))]
+
     }), 200
