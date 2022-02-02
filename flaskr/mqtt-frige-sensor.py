37400aa8454ee017f90c816f3a00e093864c1359
hirneagabriel <hirneagabriel2000@gmail.com>
2022-02-02 13:16:27 +0200

    temperature_mqtt

--- flaskr/mqtt-frige-sensor.py
+++ flaskr/mqtt-frige-sensor.py
@@ -6,10 +6,10 @@ import  requests
 
 broker = 'localhost'
 port = 1883
-topic = "door_status"
+topic = "temperature"
 
 client_id = f'python-mqtt-{random.randint(0,100)}'
-timer_set = 0
+frige_current_temp = 10.0
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -28,18 +28,9 @@ def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
         result = msg.payload.decode()
-        global timer_set
-        time_to_close = float(result)
-        timer_set = max(timer_set, time_to_close)
-        print(timer_set)
-        if time_to_close == 0:
-            pload = {'is_closed': 'True', 'time': str(timer_set)}
-            r = requests.post(url='http://localhost:5000/timer/', data=pload)
-            print(r.text)
-
-
-
-
+        set_temp = float(result)
+        if set_temp < frige_current_temp:
+            print("Cooling on")
 
     client.subscribe(topic)
     client.on_message = on_message
